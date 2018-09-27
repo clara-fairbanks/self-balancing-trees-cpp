07f9b194107b057754bfc86c08a1498fa2ae181a
Clara Fairbanks <clarafairbanks@claras-macbook-pro.local>
2018-09-27 19:06:03 -0400

    Initial Commit

---
+++ AVLTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AVLTree
+//
+//  Created by Clara Fairbanks on 9/27/18.
+//  Copyright © 2018 Clara Fairbanks. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
